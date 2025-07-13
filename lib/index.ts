155057132d43ceeb785bb2a2f693a294beb718be
Chris Engelsma <chris.engelsma@gmail.com>
2025-07-12 20:49:27 -0500

    Testing export of style module;

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,9 @@
+
 export { FramePanel  } from './components/FramePanel/FramePanel.tsx';
 export { OvalButton  } from './components/OvalButton/OvalButton.tsx';
 export { GridView  } from './components/GridView/GridView.tsx';
 export { CalendarGrid  } from './components/CalendarGrid/CalendarGrid.tsx';
 
 export { ThemeProvider } from './components/ThemeProvider/ThemeProvider.tsx';
+
+export * as styles from './styles/_export.module.scss';
